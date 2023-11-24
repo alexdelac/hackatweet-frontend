71fdcd3dceb963f4fcd66d184ee4d18480e1083f
David <evil.dave59@gmail.com>
2023-11-24 17:25:27 +0100

    finalisation css

--- components/TrendMess.js
+++ components/TrendMess.js
@@ -1,10 +1,10 @@
 import styles from "../styles/TrendMess.module.css";
 
-function TrendMess() {
+function TrendMess(props) {
   return (
     <div className={styles.container}>
-      <h5 className={styles.hashtag}>#hashtag</h5>
-      <p className={styles.counterTweet}>counter Tweet</p>
+      <h5 className={styles.hashtag}>{props.name}</h5>
+      <p className={styles.counterTweet}>{props.count} tweet</p>
     </div>
   );
 }
