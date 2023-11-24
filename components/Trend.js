a8400b8196bd96872aeadda2cacd9ee132a8a4a0
David <evil.dave59@gmail.com>
2023-11-24 16:47:16 +0100

    style trendmess

--- components/Trend.js
+++ components/Trend.js
@@ -1,10 +1,12 @@
 import styles from "../styles/Trend.module.css";
+import TrendMess from "./TrendMess";
 
 function Trend() {
   return (
     <aside className={styles.container}>
       <h2 className={styles.title}>TREND COMPONENT</h2>
       <div className={styles.content}></div>
+      <TrendMess />
     </aside>
   );
 }
